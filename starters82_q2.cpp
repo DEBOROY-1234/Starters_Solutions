c5130cd0b7c5b43cb788016c571fff5b1ad6dcc7
DEBOROY-1234 <deboroy2002@gmail.com>
2023-05-05 12:49:16 +0530

    Reach Home

---
+++ starters82_q2.cpp
@@ -0,0 +1,57 @@
+//Reach Home
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    int n = 5*x;
+	    
+	    if(n>=y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    int n = 5*x;
+	    
+	    if(n>=y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
