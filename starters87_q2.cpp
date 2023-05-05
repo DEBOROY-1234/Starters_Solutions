e4c951a0a33504d2280dbf5306115d75e1e8915d
DEBOROY-1234 <deboroy2002@gmail.com>
2023-05-05 13:16:42 +0530

    Buy Ten Packets

---
+++ starters87_q2.cpp
@@ -0,0 +1,20 @@
+//Buy Ten Packets
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	    int x,y;
+	    cin>>x>>y;
+	    
+	    int t = x + y*2;
+	    
+	    cout<<t<<endl;
+	}
+	return 0;
+}
