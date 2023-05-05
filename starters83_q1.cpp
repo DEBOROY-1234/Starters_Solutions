b2bc26791ac2d870b80562c8d2ef097f025e9836
DEBOROY-1234 <deboroy2002@gmail.com>
2023-05-05 12:54:38 +0530

    Couple Game

---
+++ starters83_q1.cpp
@@ -0,0 +1,22 @@
+//Couple Game
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	    int b,g;
+	    
+	    cin>>g>>b;
+	    
+	    int x = b-g;
+	    
+	    cout<<x<<endl;
+	    
+	}
+	return 0;
+}
