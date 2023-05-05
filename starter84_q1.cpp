114fcdf2fef9fbdf9ad69ab6e2beb05050676633
DEBOROY-1234 <deboroy2002@gmail.com>
2023-05-05 13:00:31 +0530

    IPL Ticket Rush

---
+++ starter84_q1.cpp
@@ -0,0 +1,23 @@
+//IPL Ticket Rush
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--){
+	    
+	    int n,m;
+	    cin>>n>>m;
+	    
+	    if(n>m){
+	        cout<<n-m<<endl;
+	    }
+	    else{
+	        cout<<"0"<<endl;
+	    }
+	    
+	}
+	return 0;
+}
